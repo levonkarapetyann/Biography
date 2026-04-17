5398f52c33e6fcb0c7f231d64fbc749e014a48a6
Levon <lyov33333@gmail.com>
2026-04-17 15:39:42 +0400

    Update

--- app.py
+++ app.py
@@ -1,15 +1,14 @@
 from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'my_secret_key'
+socketio = SocketIO(app, cors_allowed_origns="*")
 
 @app.route('/home')
 @app.route('/')
 def index():
-    return render_template('main.html')
-
-
-
+    return render_template('levon_wiki.html')
 
 if __name__ == '__main__':
-     app.run(debug=True)
-     
+    socketio.run(app, debug=True, port=5000) 
